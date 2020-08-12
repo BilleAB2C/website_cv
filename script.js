43200fde3fe323b0f52b68dee627a6bad625466c
Billel Abbes <32451579+BilleAB2C@users.noreply.github.com>
2020-08-12 17:40:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+function intro() {
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='block';
+	cursus.style.display='none';
+	projets.style.display='none';
+	exper.style.display='none';
+	competence.style.display='none';
+	caract.style.display='none';
+	contact.style.display='none';
+}
+
+
+function cursus() {
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='block';
+	projets.style.display='none';
+	exper.style.display='none';
+	competence.style.display='none';
+	caract.style.display='none';
+	contact.style.display='none';
+}
+
+function projets(){
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='none';
+	projets.style.display='block';
+	exper.style.display='none';
+	competence.style.display='none';
+	caract.style.display='none';
+	contact.style.display='none';
+}
+
+function expr() {
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='none';
+	projets.style.display='none';
+	exper.style.display='block';
+	competence.style.display='none';
+	caract.style.display='none';
+	contact.style.display='none';
+}
+
+function competence() {
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='none';
+	projets.style.display='none';
+	exper.style.display='none';
+	competence.style.display='block';
+	caract.style.display='none';
+	contact.style.display='none';
+}
+
+function caract() {
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='none';
+	projets.style.display='none';
+	exper.style.display='none';
+	competence.style.display='none';
+	caract.style.display='block';
+	contact.style.display='none';
+}
+
+function contact(){
+	var intro = document.getElementById('intro'); 
+	var cursus = document.getElementById('cursus');
+	var projets = document.getElementById('projets');
+	var exper = document.getElementById('exper');
+	var competence = document.getElementById('competence');
+	var caract = document.getElementById('caract');	
+	var contact = document.getElementById('contact');
+
+	intro.style.display='none';
+	cursus.style.display='none';
+	projets.style.display='none';
+	exper.style.display='none';
+	competence.style.display='none';
+	caract.style.display='none';
+	contact.style.display='block';
+
+}
